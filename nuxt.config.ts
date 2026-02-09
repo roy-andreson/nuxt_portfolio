87d8075394ba4e22848dabb7b50277f4cad2c456
Roy Andreson <bigchubby666@gmail.com>
2026-02-09 10:57:56 -0500

    feat: add i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   // Nuxt 4 does not resolve aliases like ~ or @ in this css array,
   // so we use a relative path from the project root instead.
   // css: ['./assets/css/tailwind.css'],
@@ -11,5 +11,13 @@ export default defineNuxtConfig({
       supabaseUrl: process.env.SUPABASE_URL,
       supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY,
     },
+  },
+  i18n: {
+    locales: [
+      {'code': 'en', language: 'en-US', },
+      {'code': 'es', language: 'fr-FR',},
+    ],
+    defaultLocale: 'en',
+    vueI18n: './i18n.config.ts',
   },
 })
